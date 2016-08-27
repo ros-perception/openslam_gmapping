369001791cc5640fc2ad91b68ad71d53c070e8ed
Bo Wang <david.b.wang@gmail.com>
2016-08-26 18:31:03 -0700

    Re-enabled configfile_test

--- configfile/configfile_test.cpp
+++ configfile/configfile_test.cpp
@@ -2,47 +2,45 @@
  *
  * This file is part of the GMAPPING project
  *
- * GMAPPING Copyright (c) 2004 Giorgio Grisetti, 
+ * GMAPPING Copyright (c) 2004 Giorgio Grisetti,
  * Cyrill Stachniss, and Wolfram Burgard
  *
- * This software is licensed under the "Creative Commons 
- * License (Attribution-NonCommercial-ShareAlike 2.0)" 
- * and is copyrighted by Giorgio Grisetti, Cyrill Stachniss, 
+ * This software is licensed under the "Creative Commons
+ * License (Attribution-NonCommercial-ShareAlike 2.0)"
+ * and is copyrighted by Giorgio Grisetti, Cyrill Stachniss,
  * and Wolfram Burgard.
- * 
+ *
  * Further information on this license can be found at:
  * http://creativecommons.org/licenses/by-nc-sa/2.0/
- * 
+ *
  * GMAPPING is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied 
+ * but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  
+ * PURPOSE.
  *
  *****************************************************************/
 
-
-
 #include <iostream>
-#include "configfile.h"
+#include <configfile/configfile.h>
 
 using namespace std;
 using namespace GMapping;
 
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv)
+{
   if (argc != 2) {
     cerr << "Usage:  " << argv[0] << " [initifle]" << endl;
     exit(0);
   }
-  
+
   ConfigFile cfg;
-  cfg.read(argv[argc-1]);
+  cfg.read(argv[argc - 1]);
 
   cout << "-- values from configfile --" << endl;
   cfg.dumpValues(cout);
 
   cout << "-- adding a value --" << endl;
-  cfg.value("unkown","unkown","the new value!");
+  cfg.value("unkown", "unkown", "the new value!");
 
   cout << "-- values from configfile & added values --" << endl;
   cfg.dumpValues(cout);
