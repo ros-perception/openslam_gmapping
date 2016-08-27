8dff6281921c411ffd74a1a74c42bfbe0d0ebc7b
Bo Wang <david.b.wang@gmail.com>
2016-08-26 18:36:47 -0700

    Reformatted configfile

--- configfile/configfile.cpp
+++ configfile/configfile.cpp
@@ -2,148 +2,163 @@
  *
  * This file is part of the GMAPPING project
  *
- * GMAPPING Copyright (c) 2004 Giorgio Grisetti, 
+ * GMAPPING Copyright (c) 2004 Giorgio Grisetti,
  * Cyrill Stachniss, and Wolfram Burgard
  *
- * This software is licensed under the "Creative Commons 
- * License (Attribution-NonCommercial-ShareAlike 2.0)" 
- * and is copyrighted by Giorgio Grisetti, Cyrill Stachniss, 
+ * This software is licensed under the "Creative Commons
+ * License (Attribution-NonCommercial-ShareAlike 2.0)"
+ * and is copyrighted by Giorgio Grisetti, Cyrill Stachniss,
  * and Wolfram Burgard.
- * 
+ *
  * Further information on this license can be found at:
  * http://creativecommons.org/licenses/by-nc-sa/2.0/
- * 
+ *
  * GMAPPING is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied 
+ * but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  
+ * PURPOSE.
  *
  *****************************************************************/
 
-
-#include "configfile/configfile.h"
-
+#include <configfile/configfile.h>
 #include <fstream>
 #include <iostream>
-
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <ctype.h>
 
-namespace GMapping{
+namespace GMapping
+{
+
 using namespace std;
 
-AutoVal::AutoVal(std::string const& value) {
-  m_value=value;
+AutoVal::AutoVal(std::string const& value)
+{
+  m_value = value;
 }
 
-AutoVal::AutoVal(const char* c) {
-  m_value=c;
+AutoVal::AutoVal(const char* c)
+{
+  m_value = c;
 }
 
-AutoVal::AutoVal(double d) {
+AutoVal::AutoVal(double d)
+{
   std::stringstream s;
-  s<<d;
-  m_value=s.str();
+  s << d;
+  m_value = s.str();
 }
 
-AutoVal::AutoVal(bool d) {
+AutoVal::AutoVal(bool d)
+{
   std::stringstream s;
-  if(d)
+  if (d)
     s << "on";
-  else 
+  else
     s << "off";
-  m_value=s.str();
+  m_value = s.str();
 }
 
-
-AutoVal::AutoVal(int i) {
+AutoVal::AutoVal(int i)
+{
   std::stringstream s;
-  s<<i;
-  m_value=s.str();
+  s << i;
+  m_value = s.str();
 }
 
-AutoVal::AutoVal(unsigned int i) {
+AutoVal::AutoVal(unsigned int i)
+{
   std::stringstream s;
-  s<<i;
-  m_value=s.str();
+  s << i;
+  m_value = s.str();
 }
 
-AutoVal::AutoVal(AutoVal const& other) {
-  m_value=other.m_value;
+AutoVal::AutoVal(AutoVal const& other)
+{
+  m_value = other.m_value;
 }
 
-AutoVal& AutoVal::operator=(AutoVal const& other) {
-  m_value=other.m_value;
+AutoVal& AutoVal::operator=(AutoVal const& other)
+{
+  m_value = other.m_value;
   return *this;
 }
 
-AutoVal& AutoVal::operator=(double d) {
+AutoVal& AutoVal::operator=(double d)
+{
   std::stringstream s;
   s << d;
   m_value = s.str();
   return *this;
 }
 
-AutoVal& AutoVal::operator=(bool d) {
+AutoVal& AutoVal::operator=(bool d)
+{
   std::stringstream s;
-  if(d)
+  if (d)
     s << "on";
-  else 
+  else
     s << "off";
   m_value = s.str();
   return *this;
 }
 
-
-AutoVal& AutoVal::operator=(int i) {
+AutoVal& AutoVal::operator=(int i)
+{
   std::stringstream s;
   s << i;
   m_value = s.str();
   return *this;
 }
 
-AutoVal& AutoVal::operator=(unsigned int i) {
+AutoVal& AutoVal::operator=(unsigned int i)
+{
   std::stringstream s;
   s << i;
   m_value = s.str();
   return *this;
 }
 
-
-AutoVal& AutoVal::operator=(std::string const& s) {
-  m_value=s;
+AutoVal& AutoVal::operator=(std::string const& s)
+{
+  m_value = s;
   return *this;
 }
 
-AutoVal::operator std::string() const {
+AutoVal::operator std::string() const
+{
   return m_value;
 }
 
-AutoVal::operator double() const {
+AutoVal::operator double() const
+{
   return atof(m_value.c_str());
 }
 
-AutoVal::operator int() const {
+AutoVal::operator int() const
+{
   return atoi(m_value.c_str());
 }
 
-AutoVal::operator unsigned int() const {
+AutoVal::operator unsigned int() const
+{
   return (unsigned int) atoi(m_value.c_str());
 }
 
-AutoVal::operator bool() const {
+AutoVal::operator bool() const
+{
   // stupid c++ does not allow compareNoCase...
-  if (toLower(m_value)=="on" || atoi(m_value.c_str()) == 1)
+  if (toLower(m_value) == "on" || atoi(m_value.c_str()) == 1)
     return true;
   return false;
 }
 
-std::string AutoVal::toLower(std::string const& source) const {
+std::string AutoVal::toLower(std::string const& source) const
+{
   std::string result(source);
-  char c='\0';
-  for (unsigned int i=0; i<result.length(); i++) {
+  char c = '\0';
+  for (unsigned int i = 0; i < result.length(); i++) {
     c = result[i];
     c = ::tolower(c);
     result[i] = c;
@@ -151,17 +166,19 @@ std::string AutoVal::toLower(std::string const& source) const {
   return result;
 }
 
-
 //////////////////////////////////////////////////////////
 
-ConfigFile::ConfigFile() {
+ConfigFile::ConfigFile()
+{
 }
 
-ConfigFile::ConfigFile(std::string const& configFile) {
+ConfigFile::ConfigFile(std::string const& configFile)
+{
   read(configFile);
 }
 
-bool ConfigFile::read(std::string const& configFile) {
+bool ConfigFile::read(std::string const& configFile)
+{
   std::ifstream file(configFile.c_str());
 
   if (!file || file.eof())
@@ -171,124 +188,144 @@ bool ConfigFile::read(std::string const& configFile) {
   std::string name;
   std::string val;
   std::string inSection;
-  while (std::getline(file,line)) {
+  while (std::getline(file, line)) {
 
-    if (! line.length()) continue;
+    if (!line.length())
+      continue;
     // cute the comments
-    if (line[0] == '#') continue;
-    line = truncate(line,"#");
+    if (line[0] == '#')
+      continue;
+    line = truncate(line, "#");
     line = trim(line);
-    if (! line.length()) continue;
-    
+    if (!line.length())
+      continue;
+
     if (line[0] == '[') {
-      inSection=trim(line.substr(1,line.find(']')-1));
+      inSection = trim(line.substr(1, line.find(']') - 1));
       continue;
     }
-    
+
     istringstream lineStream(line);
     lineStream >> name;
     lineStream >> val;
-    insertValue(inSection,name,val);
+    insertValue(inSection, name, val);
   }
   return true;
 }
 
-void ConfigFile::insertValue(std::string const& section, std::string const& entry, std::string const& thevalue)  {
-  m_content[toLower(section)+'/'+toLower(entry)]=AutoVal(thevalue);
+void ConfigFile::insertValue(std::string const& section, std::string const& entry, std::string const& thevalue)
+{
+  m_content[toLower(section) + '/' + toLower(entry)] = AutoVal(thevalue);
 }
 
-AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry) const {
+AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry) const
+{
 
-  std::map<std::string,AutoVal>::const_iterator ci = 
-    m_content.find(toLower(section) + '/' + toLower(entry));
-  if (ci == m_content.end()) throw "entry does not exist";
+  std::map<std::string, AutoVal>::const_iterator ci =
+      m_content.find(toLower(section) + '/' + toLower(entry));
+  if (ci == m_content.end())
+    throw "entry does not exist";
 
   return ci->second;
 }
 
-AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, double def) {
+AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, double def)
+{
   try {
     return value(section, entry);
-  } catch(const char *) {
-    return m_content.insert(std::make_pair(section+'/'+entry, AutoVal(def))).first->second;
+  }
+  catch (const char *) {
+    return m_content.insert(std::make_pair(section + '/' + entry, AutoVal(def))).first->second;
   }
 }
 
-AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, bool def) {
+AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, bool def)
+{
   try {
     return value(section, entry);
-  } catch(const char *) {
-    return m_content.insert(std::make_pair(section+'/'+entry, AutoVal(def))).first->second;
+  }
+  catch (const char *) {
+    return m_content.insert(std::make_pair(section + '/' + entry, AutoVal(def))).first->second;
   }
 }
 
-AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, int def) {
+AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, int def)
+{
   try {
     return value(section, entry);
-  } catch(const char *) {
-    return m_content.insert(std::make_pair(section+'/'+entry, AutoVal(def))).first->second;
+  }
+  catch (const char *) {
+    return m_content.insert(std::make_pair(section + '/' + entry, AutoVal(def))).first->second;
   }
 }
 
-AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, unsigned int def) {
+AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, unsigned int def)
+{
   try {
     return value(section, entry);
-  } catch(const char *) {
-    return m_content.insert(std::make_pair(section+'/'+entry, AutoVal(def))).first->second;
+  }
+  catch (const char *) {
+    return m_content.insert(std::make_pair(section + '/' + entry, AutoVal(def))).first->second;
   }
 }
 
-AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, std::string const& def) {
+AutoVal const& ConfigFile::value(std::string const& section, std::string const& entry, std::string const& def)
+{
   try {
     return value(section, entry);
-  } catch(const char *) {
-    return m_content.insert(std::make_pair(section+'/'+entry, AutoVal(def))).first->second;
+  }
+  catch (const char *) {
+    return m_content.insert(std::make_pair(section + '/' + entry, AutoVal(def))).first->second;
   }
 }
 
-void ConfigFile::dumpValues(std::ostream& out) {
+void ConfigFile::dumpValues(std::ostream& out)
+{
 
-  for (std::map<std::string,AutoVal>::const_iterator it = m_content.begin();
-       it != m_content.end(); it++) {
-    out << (std::string) it->first << " " << (std::string)it->second << std::endl;
+  for (std::map<std::string, AutoVal>::const_iterator it = m_content.begin();
+      it != m_content.end(); it++) {
+    out << (std::string) it->first << " " << (std::string) it->second << std::endl;
   }
 }
 
-
-std::string ConfigFile::trim(std::string const& source, char const* delims) const {
+std::string ConfigFile::trim(std::string const& source, char const* delims) const
+{
   std::string result(source);
   std::string::size_type index = result.find_last_not_of(delims);
-  if(index != std::string::npos)
+  if (index != std::string::npos)
     result.erase(++index);
 
   index = result.find_first_not_of(delims);
-  if(index != std::string::npos)
+  if (index != std::string::npos)
     result.erase(0, index);
   else
     result.erase();
   return result;
 }
 
-std::string ConfigFile::truncate(std::string const& source, const char* atChar) const {
+std::string ConfigFile::truncate(std::string const& source, const char* atChar) const
+{
   std::string::size_type index = source.find_first_of(atChar);
 
-  if(index == 0) 
+  if (index == 0)
     return "";
-  else if(index == std::string::npos) {
+  else if (index == std::string::npos) {
     return source;
   }
-  return 
-    source.substr(0,index);
+  return
+  source.substr(0, index);
 }
 
-std::string ConfigFile::toLower(std::string const& source) const {
+std::string ConfigFile::toLower(std::string const& source) const
+{
   std::string result(source);
-  char c='\0';
-  for (unsigned int i=0; i<result.length(); i++) {
+  char c = '\0';
+  for (unsigned int i = 0; i < result.length(); i++) {
     c = result[i];
     c = ::tolower(c);
     result[i] = c;
   }
   return result;
 }
-};
+
+}
--- include/gmapping/configfile/configfile.h
+++ include/gmapping/configfile/configfile.h
@@ -2,24 +2,23 @@
  *
  * This file is part of the GMAPPING project
  *
- * GMAPPING Copyright (c) 2004 Giorgio Grisetti, 
+ * GMAPPING Copyright (c) 2004 Giorgio Grisetti,
  * Cyrill Stachniss, and Wolfram Burgard
  *
- * This software is licensed under the "Creative Commons 
- * License (Attribution-NonCommercial-ShareAlike 2.0)" 
- * and is copyrighted by Giorgio Grisetti, Cyrill Stachniss, 
+ * This software is licensed under the "Creative Commons
+ * License (Attribution-NonCommercial-ShareAlike 2.0)"
+ * and is copyrighted by Giorgio Grisetti, Cyrill Stachniss,
  * and Wolfram Burgard.
- * 
+ *
  * Further information on this license can be found at:
  * http://creativecommons.org/licenses/by-nc-sa/2.0/
- * 
+ *
  * GMAPPING is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied 
+ * but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  
+ * PURPOSE.
  *
  *****************************************************************/
-
 
 #ifndef CONFIGFILE_H
 #define CONFIGFILE_H
@@ -28,84 +27,68 @@
 #include <string>
 #include <map>
 
-namespace GMapping{
+namespace GMapping
+{
 
-class AutoVal {
-public:
-  AutoVal() {};
-  explicit AutoVal(const std::string&);
-  explicit AutoVal(double);
-  explicit AutoVal(int);
-  explicit AutoVal(unsigned int);
-  explicit AutoVal(bool);
-  explicit AutoVal(const char*);
+class AutoVal
+{
+  public:
+    AutoVal() {}
+    explicit AutoVal(const std::string&);
+    explicit AutoVal(double);
+    explicit AutoVal(int);
+    explicit AutoVal(unsigned int);
+    explicit AutoVal(bool);
+    explicit AutoVal(const char*);
 
-  AutoVal(const AutoVal&);
-  AutoVal& operator=(AutoVal const&);
+    AutoVal(const AutoVal&);
+    AutoVal& operator=(AutoVal const&);
 
-  AutoVal& operator=(double);
-  AutoVal& operator=(int);
-  AutoVal& operator=(unsigned int);
-  AutoVal& operator=(bool);
-  AutoVal& operator=(std::string const&);
+    AutoVal& operator=(double);
+    AutoVal& operator=(int);
+    AutoVal& operator=(unsigned int);
+    AutoVal& operator=(bool);
+    AutoVal& operator=(std::string const&);
 
-public:
-  operator std::string() const;
-  operator double() const;
-  operator int() const;
-  operator unsigned int() const;
-  operator bool() const;
+    operator std::string() const;
+    operator double() const;
+    operator int() const;
+    operator unsigned int() const;
+    operator bool() const;
 
-protected:
-  std::string toLower(std::string const& source) const;
+  protected:
+    std::string toLower(std::string const& source) const;
 
-private:
-  std::string m_value;
+  private:
+    std::string m_value;
 };
 
-class ConfigFile {
-  std::map<std::string,AutoVal> m_content;
-  
-public:
-  ConfigFile();
-  ConfigFile(std::string const& configFile);
+class ConfigFile
+{
+    std::map<std::string, AutoVal> m_content;
 
-  bool read(std::string const& configFile);
-  
- 
-  AutoVal const& value(std::string const& section, 
-			 std::string const& entry) const;
+  public:
+    ConfigFile();
+    ConfigFile(std::string const& configFile);
 
-  AutoVal const& value(std::string const& section, 
-			 std::string const& entry, 
-			 double def);
+    bool read(std::string const& configFile);
 
-  AutoVal const& value(std::string const& section, 
-			 std::string const& entry, 
-			 bool def);
+    AutoVal const& value(std::string const& section, std::string const& entry) const;
+    AutoVal const& value(std::string const& section, std::string const& entry, double def);
+    AutoVal const& value(std::string const& section, std::string const& entry, bool def);
+    AutoVal const& value(std::string const& section, std::string const& entry, int def);
+    AutoVal const& value(std::string const& section, std::string const& entry, unsigned int def);
+    AutoVal const& value(std::string const& section, std::string const& entry, std::string const& def);
 
-  AutoVal const& value(std::string const& section, 
-			 std::string const& entry, 
-			 int def);
+    void dumpValues(std::ostream& out);
 
-  AutoVal const& value(std::string const& section, 
-			 std::string const& entry, 
-			 unsigned int def);
-
-  AutoVal const& value(std::string const& section, 
-			 std::string const& entry, 
-			 std::string const& def);
-
-  void dumpValues(std::ostream& out);
-
-
- protected:
-  std::string trim(std::string const& source, char const* delims = " \t\r\n") const;
-  std::string truncate(std::string const& source, const char* atChar) const;
-  std::string toLower(std::string const& source) const;
-  void insertValue(std::string const& section, std::string const& entry, std::string const& thevalue );
+  protected:
+    std::string trim(std::string const& source, char const* delims = " \t\r\n") const;
+    std::string truncate(std::string const& source, const char* atChar) const;
+    std::string toLower(std::string const& source) const;
+    void insertValue(std::string const& section, std::string const& entry, std::string const& thevalue);
 };
 
-};
+}
 
 #endif
