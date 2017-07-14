e7e2916773b84a0668e7d6259f18807549dfd16f
Hunter L. Allen <hunter@openrobotics.org>
2017-07-14 11:05:39 -0700

    This bothers me that I have no idea whether or not the out++ is meant to be in the if statement.

--- include/gmapping/particlefilter/particlefilter.h
+++ include/gmapping/particlefilter/particlefilter.h
@@ -137,10 +137,9 @@ void rle(OutputIterator& out, const Iterator & begin, const Iterator & end){
 			count=1;
 		}
 	}
-	if (count>0) {
+	if (count>0)
 		*out=std::make_pair(current,count);
-        out++;  // WARNING(allenh1): Should this be in the if stmt?
-    }
+    out++;  // WARNING(allenh1): Bad indentation. Place in if?
 }
 
 //BEGIN legacy
